9432e57fb2ffe946929de81b00f2fa2fd55bb00c
igstudija <artis@igstudija.lv>
2021-08-06 14:54:55 +0300

    Button toogle

--- patient-data/page.php
+++ patient-data/page.php
@@ -44,7 +44,9 @@ include '../includes/header.php'; ?>
         <td><a href="#">Pārtikas alerģija</a></td>
         <td class="kods"><span>A73</span></td>
         <td>Kivi</td>
-        <td><a class="btn btn-primary btn-sm" data-bs-toggle="collapse" href="#link_1" role="button" aria-expanded="false" aria-controls="link_1">Parādīt <i class="far fa-caret-square-down"></i></a></td>
+        <td><a class="btn btn-primary btn-sm" data-bs-toggle="collapse" href="#link_1" role="button" aria-expanded="false" aria-controls="link_1">
+          <span class="open">Parādīt</span><span class="close">Aizvērt</span>
+          <i class="far fa-caret-square-down"></i></a></td>
       </tr>
       <tr class="collapse" id="link_1">
       <td colspan="5" class="nested"><?php include 'nested-table.php'; ?></td>
@@ -57,7 +59,9 @@ include '../includes/header.php'; ?>
         <td><a href="#">Medikamentu alerģija</a></td>
         <td class="kods"><span>B13</span></td>
         <td>Paracetamols</td>
-        <td><a class="btn btn-primary btn-sm" data-bs-toggle="collapse" href="#link_2" role="button" aria-expanded="false" aria-controls="link_2">Parādīt <i class="far fa-caret-square-down"></i></a></td>
+        <td><a class="btn btn-primary btn-sm" data-bs-toggle="collapse" href="#link_2" role="button" aria-expanded="false" aria-controls="link_2">
+<span class="open">Parādīt</span><span class="close">Aizvērt</span>
+          <i class="far fa-caret-square-down"></i></a></td>
       </tr>
       <tr class="collapse" id="link_2">
       <td colspan="5" class="nested"><?php include 'nested-table.php'; ?></td>
